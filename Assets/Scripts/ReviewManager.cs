d23da2b09feb83fd763d5d05fbe8dbb8bdb9dc6f
KwonBoWon <bogod1207@naver.com>
2023-06-03 15:56:18 +0900

    리뷰 로드 구현완료(리뷰데이터 만들어서 넣어야함)

--- Assets/Scripts/ReviewManager.cs
+++ Assets/Scripts/ReviewManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -17,31 +18,61 @@ public class ReviewManager : MonoBehaviour
     public TextMeshProUGUI shopName;
     public TMP_InputField inputField;
     private MainManager mainManager;
+    private string path;
     private float rating;
+    private ReviewData review;
 
     void Start()
     {
         mainManager = MainManager.Instance;
         shopName.text = mainManager.realShopName;
+        review = new ReviewData();
+        review.userName = mainManager.name;
+        review.shopName = mainManager.shopName;
+        path = Path.Combine(Application.dataPath, "reviewData.json");
     }
     public void SliderButton()
     {
         rating = ratingSlider.value; 
-        ratingText.text = (Mathf.Floor(rating * 10) * 0.1f).ToString(); // º“ºˆ¡° µ—¬∞¿⁄∏Æø°º≠ πˆ∏≤
-
+        ratingText.text = (Mathf.Floor(rating * 10) * 0.1f).ToString(); 
+        review.rating = rating;
     }
-    public void SaveToJson()
-    {
-        string inputString = inputField.text;
-        JSONObject data = new JSONObject();
-        data["review"] = inputString;
-        data["shopName"] = mainManager.shopName;
-        data["userName"] = mainManager.userName;
-        data["rating"] = rating;
-        string jsonString = data.ToString();
-        //File.WriteAllText(Path.Combine(Application.dataPath, "reviewData.json"), jsonString);
-        File.AppendAllText(Path.Combine(Application.dataPath, "reviewData.json"), jsonString);
+    public void InputFiled(){
+        review.review = inputField.text;
+    }
 
+
+    public void Write(){
+        ReviewDataList existingData = LoadData();
+        Debug.Log(existingData);
+        existingData.reviews.Add(review);
+        SaveData(existingData);
         SceneManager.LoadScene("ShopInfo");
+    }
+    private ReviewDataList LoadData()
+    {
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            return JsonUtility.FromJson<ReviewDataList>(json);
+        }
+        else
+        {
+            return new ReviewDataList();
+        }
+    }
+    public void SaveData(ReviewDataList data)
+    {
+        try
+        {
+            string json = JsonUtility.ToJson(data);
+            File.WriteAllText(path, json);
+            Debug.Log("Îç∞Ïù¥ÌÑ∞ Ï†ÄÏû• ÏôÑÎ£å");
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Îç∞Ïù¥ÌÑ∞ Ï†ÄÏû• Ïã§Ìå®: " + e.Message);
+        }
+
     }
 }
